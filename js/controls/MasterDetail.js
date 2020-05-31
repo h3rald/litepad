4869e11ac07935a83b3992473c097b2aeb842b76
h3rald <h3rald@h3rald.com>
2020-05-31 11:06:48 +0200

    Fixed task updates.

--- js/controls/MasterDetail.js
+++ js/controls/MasterDetail.js
@@ -8,92 +8,14 @@ import Field from "./Field.js";
 import Snippet from "../models/Snippet.js";
 import Note from "../models/Item.js";
 import { saveItem, getItems } from "../services/api.js";
-
-const types = {
-  snippets: Snippet,
-  notes: Note,
-};
+import { handleUpdateTask } from "../services/gfmtasks.js";
 
 const state = {};
-
-// Marked extension
-const renderer = {
-  list(body, ordered, start, token) {
-    let content = body;
-    if (content.match(/type="checkbox">/)) {
-      while (content.match(/disabled(="")?/)) {
-        content = content.replace(/disabled(="")?/, "");
-      }
-      return `<ul class="checklist" data-raw="${token.raw}">${content}</ul>`;
-    }
-    return false;
-  },
-};
-marked.use({ renderer });
-
-// Task persistence
-const updateTask = async (event) => {
-  const item = h3.state.item;
-  const checkbox = event.currentTarget;
-  const li = checkbox.parentNode;
-  const ul = li.parentNode;
-  let list = ul;
-  while (list) {
-    if (list.parentNode.tagName === "LI") {
-      list = list.parentNode.parentNode;
-    } else {
-      break;
-    }
-  }
-  const taskList = list.dataset.raw;
-  let line = -1;
-  const walkList = (list) => {
-    for (let item of list.childNodes) {
-      if (item.tagName === "LI") {
-        line++;
-      }
-      if (item === li) {
-        return true;
-      } else {
-        for (let liChild of item.childNodes) {
-          if (liChild.tagName === "UL") {
-            if (walkList(liChild)) {
-              return true;
-            }
-          }
-        }
-      }
-    }
-    return false;
-  };
-  walkList(list);
-  const newTaskList = taskList
-    .split("\n")
-    .map((t, i) => {
-      if (i === line) {
-        return t.replace(/\[.\]\s+/, checkbox.checked ? "[x] " : "[ ] ");
-      }
-      return t;
-    })
-    .join("\n");
-  const regexp = new RegExp(
-    taskList.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&").replace(/\s+/gm, "\\s+"),
-    "m"
-  );
-  list.dataset.raw = newTaskList;
-  const text = item.data.text.replace(regexp, newTaskList);
-  const newItem = await saveItem(...item.id.split("/"), { ...item.data, text });
-  h3.dispatch("item/set", newItem);
-  const newItems = await getItems(h3.state.collection, h3.state.query);
-  h3.dispatch("items/set", newItems.results);
-  h3.dispatch("total/set", newItems.total);
-  h3.redraw();
-};
 
 export default ({ items, item, collection, add }) => {
   let data;
   if (item) {
-    data = new types[collection]();
+    data = new h3.state.config.types[collection]();
     data.set(item);
   }
   state.current = state.current || {};
@@ -123,19 +45,12 @@ export default ({ items, item, collection, add }) => {
         ),
         item
           ? h3("div.detail.pl-4.d-flex.flex-column.flex-1", [
-              h3("h2.item-title", item.data.title),
+              h3("h1.item-title", item.data.title),
               h3("div.d-flex.flex-column.flex-1.scrollable-area", [
                 collection === "notes" &&
                   h3("div.markdown", {
                     $html: marked(DOMPurify.sanitize(item.data.text)),
-                    $onrender: (node) => {
-                      const tasks = node.querySelectorAll(
-                        ".checklist li input"
-                      );
-                      tasks.forEach((task) =>
-                        task.addEventListener("change", updateTask)
-                      );
-                    },
+                    $onrender: handleUpdateTask,
                   }),
                 collection === "snippets" &&
                   Field({ ...data.code, editable: false }),
