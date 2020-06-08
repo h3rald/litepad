a7edab005df596aded43d9c0e74f5fb18f838dd0
h3rald <h3rald@h3rald.com>
2020-06-08 16:31:12 +0200

    Improved dowoloadable HTML notes.

--- js/services/download.js
+++ js/services/download.js
@@ -1,5 +1,30 @@
 import h3 from "../h3.js";
 
+const basicCss = `
+body {
+    margin: 1rem;
+}
+h1 {
+    margin-bottom: 1rem;
+}
+h2,h3,h4,h5,h6 {
+    margin: 1rem 0 0.5rem 0;
+}
+ul,ol {
+  margin-left: 2rem;
+}
+ul.checklist {
+    list-style-type: none;
+}  
+pre[class^=language-] {
+    background: #eee;
+    border-radius: 2px;
+    border: #eee;
+    margin: 1rem 0;
+    padding: 1rem;
+}
+`;
+
 const downloadFile = (title, text, language) => {
   const lang = h3.state.config.languages[language];
   const data = new Blob([text], { type: lang.mode });
@@ -13,10 +38,36 @@ const downloadFile = (title, text, language) => {
   window.URL.revokeObjectURL(url);
 };
 
-const downloadHTML = (title, content) => {
+const addCss = (doc, css) => {
+  const style = document.createElement("style");
+  style.type = "text/css";
+  style.appendChild(document.createTextNode(css));
+  doc.head.appendChild(style);
+};
+
+const fetchCss = async (doc, file) => {
+  const css = await (await fetch(file)).text();
+  addCss(doc, css);
+};
+
+const downloadHTML = async (title, content) => {
   const doc = document.implementation.createHTMLDocument(title);
+  const h1 = doc.createElement("h1");
+  h1.appendChild(document.createTextNode(title));
   const div = doc.createElement("div");
   div.innerHTML = content;
+  await fetchCss(doc, "vendor/primer/primer.min.css");
+  addCss(doc, basicCss);
+  const viewport = document.createElement("meta");
+  viewport.name = "viewport";
+  viewport.content =
+    "width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0";
+  const generator = document.createElement("meta");
+  generator.name = "generator";
+  generator.content = "LitePad";
+  doc.head.appendChild(viewport);
+  doc.head.appendChild(generator);
+  doc.body.appendChild(h1);
   doc.body.appendChild(div);
   return downloadFile(title, doc.documentElement.outerHTML, "html");
 };
